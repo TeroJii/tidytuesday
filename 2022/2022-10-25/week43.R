fc4c63321e6a3fc3505d9c920ade7b83294be88d
TeroJii <tmjalk@gmail.com>
2022-10-25 16:49:07 +0300

    add intermediate plot

--- 2022/2022-10-25/week43.R
+++ 2022/2022-10-25/week43.R
@@ -8,9 +8,13 @@ episodes<- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
 
 ## Libraries ----
 
+library(here)
 library(tidyverse)
 library(showtext)
+library(ggimage)
 
+# define project paths
+here::i_am(path = "2022/2022-10-25/week43.R")
 
 
 ## Visualizations -----
@@ -19,6 +23,12 @@ library(showtext)
 font_add_google("Lora")
 # add font automatically
 showtext_auto()
+
+
+### Picture attribution
+# star
+attribution_star <- "I, Ssolbergj, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons"
+
 
 # get season winners
 series_winners <- bakers %>% 
@@ -31,15 +41,25 @@ names(series.label) <- 1:10
 
 
 # How did season winners fare in challenges?
-challenges %>% 
+season_winner_progression <- challenges %>% 
   filter(baker %in% series_winners$baker) %>%
   # remove David from series 1 (namesake of series 10 winner)
   filter(!(series == 1 & baker == "David")) %>% 
+  mutate(technical = if_else(is.na(technical), true = 1, false = technical))
+
+
+season_winner_progression %>% 
   ggplot(aes(x = episode, y = technical/2)) +
   geom_tile(width = 0.8, 
             aes(height = technical),
             # White filling on bars
             fill = "#FFFFFF") +
+  geom_image(data = season_winner_progression %>% filter(result == "STAR BAKER") %>%  mutate(image = here("2022", "2022-10-25", "star.png")), 
+             aes(image = image, y = technical + 1, x = episode), size = 0.05) +
+  geom_text(data = season_winner_progression %>% filter(result == "WINNER"), 
+             aes(label = result, y = technical + 3, x = episode), 
+            angle = 90,
+            color = "#FFFFFF") +
   facet_wrap(facets = "series", nrow = 2,
              # Add labels on facets
              labeller = labeller(series = series.label)) +
