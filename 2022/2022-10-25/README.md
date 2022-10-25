f629572ae6322523538e11c57c5596a8b27fa56f
TeroJii <tmjalk@gmail.com>
2022-10-25 08:44:40 +0300

    add data read in for week 43

---
+++ 2022/2022-10-25/README.md
@@ -0,0 +1,3 @@
+# Week 43 - Great British Bakeoff
+
+The data this week comes from the [`bakeoff` package](https://bakeoff.netlify.app/) from Alison Hill, Chester Ismay, and Richard Iannone.
---
+++ 2022/2022-10-25/week43.R
@@ -0,0 +1,5 @@
+## Read in data
+
+bakers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-25/bakers.csv')
+challenges <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-25/challenges.csv')
+ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-25/ratings.csv')
