846fa34ef1090c52b80b0b5b02915c3bfb98ae55
TeroJii <tmjalk@gmail.com>
2022-11-02 10:41:22 +0200

    update plot script

--- 2022/2022-11-01/week44.R
+++ 2022/2022-11-01/week44.R
@@ -1,10 +1,6 @@
 ### Tidy tuesday - 2022 week 44
 ## Author: Tero Jalkanen
 
-## Read in data
-
-horror_movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-01/horror_movies.csv')
-
 
 ## Libraries ----
 
@@ -14,14 +10,65 @@ library(showtext)
 library(tidytext)
 library(lubridate)
 library(ggrepel)
+library(textdata)
+library(rvest)
+library(tidylo)
 
 # define project paths
 here::i_am(path = "2022/2022-11-01/week44.R")
 
+## Read in data
+
+horror_movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-01/horror_movies.csv')
+
+# Iron Maiden - Fear of the Dark lyrics
+fear_of_the_dark <- read_html("https://genius.com/Iron-maiden-fear-of-the-dark-lyrics")
+
+# Extract lyrics
+fear_of_the_dark <- fear_of_the_dark %>% 
+  html_nodes("a span") %>% 
+  html_text()
+
+# Make a data.frame with the fear of the dark lyrics
+fear_df <- data.frame(verse = 1:12, lyrics = fear_of_the_dark[8:19]) %>% 
+  # remove instrumental part
+  filter(verse != 6) %>% 
+  mutate(verse = rownames(.)) %>% 
+  mutate(original_title = "Fear of the Dark",
+         release_year = 1992) %>% 
+  # add white spaces where line breaks have been
+  mutate(lyrics = gsub(pattern = "([a-z])([A-Z])", replacement = "\\1 \\2", x = lyrics))
+
+
+
+## Modify data -----
+finnish_movies <- horror_movies %>% 
+  filter(original_language == "fi") %>% 
+  mutate(release_year = lubridate::year(release_date)) %>% 
+  filter(original_title %in% c("Sauna", "Dark Floors", "Vihanpidot"))
+
+tidy_overviews <- finnish_movies %>% 
+  select(original_title, release_year, overview) %>% 
+  filter(!is.na(overview)) %>% 
+  unnest_tokens(word, input = overview)
+
+tidy_fear_of_the_dark <- fear_df %>% 
+  select(original_title, release_year, lyrics) %>% 
+  unnest_tokens(word, input = lyrics)
+
+
+word_counts <- bind_rows(tidy_overviews, tidy_fear_of_the_dark) %>%  
+  count(original_title, word, sort = TRUE)
+
+
+word_log_odds <- word_counts %>% 
+  bind_log_odds(original_title, word, n) 
+
+
+
 #############
 ## Creating the visualization -----
 #######
-
 
 ## Get fonts
 font_add_google("Frijole")
@@ -36,8 +83,8 @@ family_other <- "Kolker Brush"
 ### Picture attribution
 # star
 
-main_title <- "Placeholder"
-sub_title <- "Placeholder"
+main_title <- "Finnish Horror Movies"
+sub_title <- "Vs. Fear of the Dark"
 info_text <- "#TidyTuesday week 44, 2022 | Data: https://github.com/tashapiro/horror-movies | @JalkanenTero"
 
 
@@ -47,53 +94,44 @@ font_color <- "#ff0000"
 bg_color <- "#000000"
 
 
-
-## Modify data -----
-finnish_movies <- horror_movies %>% 
-  filter(original_language == "fi") %>% 
-  mutate(release_year = lubridate::year(release_date))
-
-tidy_overviews <- finnish_movies %>% 
-  select(original_title, release_year, overview) %>% 
-  filter(!is.na(overview)) %>% 
-  unnest_tokens(word, input = overview)
-
-
-common_words_matrix <- tidy_overviews %>% 
-  count(release_year, word, sort = TRUE) %>% 
-  anti_join(stop_words) %>% 
-  pivot_wider(names_from = "word", values_from = "n") %>% 
-  arrange(release_year) %>% 
-  column_to_rownames("release_year") %>% 
-  mutate(across(.cols = everything(), ~ replace(., is.na(.), 0)))
-
-
-pca.out <- prcomp(common_words_matrix, scale. = TRUE)
-
-
-pca.result <- pca.out$x %>% 
-  as.data.frame.matrix() %>% 
-  rownames_to_column(var = "release_year")
-
 ## Theme -----
 
-plot_theme <- theme(axis.text = element_text(color = font_color, size = 40, family = family_other),
-                    axis.title = element_text(color = font_color, size = 40, family = family_other),
-                    plot.caption = element_text(color = font_color, face = "bold", size = 15, family = family_other),
-                    plot.title = element_text(color = font_color, face = "bold", size = 105, hjust = 0.5, family = family_title),
-                    plot.subtitle = element_text(color = font_color, face = "italic", size = 95, hjust = 0.5, family = family_other),
+plot_theme <- theme(axis.text = element_text(color = font_color, size = 50, family = family_other),
+                    axis.title = element_text(color = font_color, size = 45, family = family_other),
+                    plot.caption = element_text(color = font_color, face = "bold", size = 45, family = family_other),
+                    plot.title = element_text(color = font_color, face = "bold", size = 40, hjust = 0.5, family = family_title, lineheight = 0.5),
                     plot.background = element_rect(fill = bg_color),
-                    panel.background = element_rect(fill = bg_color)
+                    panel.background = element_rect(fill = bg_color),
+                    strip.background = element_rect(fill = bg_color),
+                    strip.text = element_text(family = family_other, color = font_color, size = 50)
 )
+
+
 
 ## The plots --------
 
 
-pca.result %>% 
-  ggplot(aes(x = PC1 + 50, y = PC2 + 20)) +
-  geom_point(color = font_color) +
-  ggrepel::geom_label_repel(aes(label = release_year)) +
-  ggtitle("Test", subtitle = "test test") +
-  scale_y_log10() +
-  scale_x_log10() +
-  plot_theme
+pl <- word_log_odds %>%
+  group_by(original_title) %>%
+  slice_max(log_odds_weighted, n = 5) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, log_odds_weighted)) %>%
+  ggplot(aes(log_odds_weighted, word, fill = font_color)) +
+  geom_col(show.legend = FALSE) +
+  ggtitle(paste(main_title, sub_title)) +
+  facet_wrap(vars(original_title), scales = "free") +
+  labs(y = NULL,
+       x = "Weighted log odds",
+       caption = info_text) +
+  plot_theme 
+
+
+## Save output -----
+
+ggsave(filename = "TidyTuesday-2022-Week44.png", 
+       path = here("2022", "2022-11-01"), 
+       device = "png", 
+       units = "cm", 
+       height = 15,
+       width = 15, 
+       dpi = 300)
