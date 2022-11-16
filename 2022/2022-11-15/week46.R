f535a3e75dd8766f0e1af0d7dbbe1e2a9fe187c8
TeroJii <tmjalk@gmail.com>
2022-11-16 10:01:02 +0200

    add a boilerplate for week 46 contribution

---
+++ 2022/2022-11-15/week46.R
@@ -0,0 +1,70 @@
+### Tidy tuesday - 2022 week 46
+## Author: Tero Jalkanen
+
+
+## Libraries ----
+
+library(here)
+library(tidyverse)
+library(patchwork)
+
+# define project paths
+here::i_am(path = "2022/2022-11-15/week46.R")
+
+## Read in data
+
+
+# Or read in the data manually
+
+image_alt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-15/image_alt.csv')
+color_contrast <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-15/color_contrast.csv')
+ally_scores <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-15/ally_scores.csv')
+bytes_total <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-15/bytes_total.csv')
+speed_index <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-15/speed_index.csv')
+
+
+#############
+## Creating the visualization -----
+#######
+
+## Get fonts
+font_add_google("Black Ops One")
+font_add_google("Gruppo")
+# add font automatically
+showtext_auto()
+family_title <- "Black Ops One"
+family_other <- "Gruppo"
+
+## Texts ----
+
+### Picture attribution
+
+main_title <- "Page Metrics"
+info_text <- "#TidyTuesday week 46, 2022 | Data: httparchive.org | Viz: @TeroJii"
+
+
+### Colors ---
+
+font_color <- "#000000"
+
+
+## The plots --------
+
+title_plot <- ggplot() +
+  geom_text(data = data.frame(x = 0, y = 0, label = main_title), 
+            aes(x = x, y= y, label = label), color = font_color, size = 25, family = family_title) +
+  theme_void() +
+  theme(axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank(), text = element_text(face = "bold"))
+
+title_plot + title_plot + plot_layout(nrow = 2, heights = c(1,1))
+
+## Save output -----
+
+ggsave(filename = "TidyTuesday-2022-Week46.png", 
+       path = here("2022", "2022-11-15"), 
+       device = "png", 
+       units = "cm", 
+       height = 15,
+       width = 20, 
+       dpi = 300)
